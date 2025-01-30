95622fdcf019104eb365072b18d0a94a11defab5
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-01-30 14:12:26 +0000

    added install script

---
+++ install_script.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+sudo apt install update
+sudo apt install upgrade -y
+
+sudo apt install build-essential
+sudo apt install python3-venv
+
+
+sudo snap install nvim --classic
+
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install fish
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+
+

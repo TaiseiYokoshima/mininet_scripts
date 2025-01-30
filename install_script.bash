f8136900471f1e5d161e24ea55d979f6154dc669
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-01-30 14:15:22 +0000

    fixed install added post

--- install_script.bash
+++ install_script.bash
@@ -13,6 +13,19 @@ sudo snap install nvim --classic
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
+sudo apt install stow
+
+git clone https://github.com/TaiseiYokoshima/dotfiles $HOME/dotfiles
+
+cd $HOME/dotfiles
+
+stow fish
+stow zellij
+stow nvim
+
+
+
+
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
---
+++ post_install.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+cargo install zellij
+
+
+
+

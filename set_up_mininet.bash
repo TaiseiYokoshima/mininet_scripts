0c96cddef6a4c9681e3a28ca5c571d8aa41fe959
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-01-30 18:28:29 +0000

    added scripts to setup miniet

---
+++ set_up_mininet.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git clone https://github.com/mininet/mininet
+
+
+
+cd mininet
+
+
+git checkout -b mininet-2.3.0 2.3.0
+
+
+
+
+sed -i 's/^\(\s*\)git clone git/\1git clone https/' ./util/install.sh
+sed -i 's/\(.*\)python2 python3/\1python3/' ./util/install.sh
+
+
+cd ~
+
+
+./miniet/util/install.sh -a
+

937439f40480c2cd0efa532047ab874748ebdb16
Taisei Yokoshima <taiseiyokoshima030712@gmail.com>
2025-02-04 00:30:31 +0000

    minor changed

--- test_net.py
+++ test_net.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import CPULimitedHost, Controller, Node
 from mininet.link import TCLink
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel
+from mininet.node import CPULimitedHost, Controller, Node
+from mininet.cli import CLI
 
 
 class test_network(Topo):
@@ -23,21 +24,97 @@ def cmd(node, cmds):
         output = node.cmd(cmd)
         print(output)
 
+def ping_test(net):
+    h1 = net.get( 'h1' )
+    h2 = net.get( 'h2' )
+
+    ip = h1.IP()
+
+    CLI(net)
+
+    
+
 
 def bin_test(net):
     commands = [
+        # "mkdir -p /tmp/test_mininet/"
         "cd /home/mininet",
         "cd src",
-        "./server",
+        "./server > /tmp/server.txt &",
     ]
 
     h1 = net.get( 'h1' )
+
+    ip = h1.IP()
+
+    h2 = net.get( 'h2' )
+
+
+    print("server ip is " + ip)
+
+
+
     cmd(h1, commands)
+
+    print("ran server")
+
+    commands2 = [
+        "cd /home/mininet",
+        "cd src",
+        f"./client {ip} > /tmp/client.txt" 
+    ]
+
+
+    cmd(h2, commands2)
+
+    print("ran client")
+
+
+
+
+
+
+def rust_bin(net):
+    commands = [
+        "cd /home/mininet",
+        "mkdir -p /home/mininet/test"
+    ]
+
+    h1 = net.get( 'h1' )
+    ip = h1.IP()
+    h2 = net.get( 'h2' )
+
+
+    cmd(h1, commands)
+    cmd(h2, commands)
+
+    # print("ran server")
+
+    server = [
+        "./server_bin > /home/mininet/test/server.txt &",
+    ]
+
+    client = [
+        f"./client_bin {ip} >  /home/mininet/test/client.txt",
+    ]
+
+
+
+
+    cmd(h1, server)
+    cmd(h2, client)
+
+    # print("ran """  """client")
+
+
+
 
 
 if __name__ == '__main__':
     topo = test_network() 
     net = Mininet( topo=topo )
     net.start()
-    bin_test(net)
+    # ping_test(net)
+    # bin_test(net)
+    rust_bin(net)
 
